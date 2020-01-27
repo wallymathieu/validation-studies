61a2ec4758e5cf79c54a66da34e96f3e7ace8b0c
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-01-27 08:30:33 +0100

    C# examples

---
+++ src/CsAttributes/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CsAttributes
+{
+    public class Person
+    {
+        [MinLength(1,ErrorMessage ="NameBetween1And50"), MaxLength(50,ErrorMessage ="NameBetween1And50")]
+        public string Name{get;set;}
+        [EmailAddress(ErrorMessage ="EmailMustContainAtChar")]
+        public string Email{get;set;}
+        [Range(0,120,ErrorMessage = "AgeBetween0and120")]
+        public int Age {get;set;}
+    }
+}
---
+++ src/CsFluent/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentValidation;
+
+namespace CsFluent
+{
+    public class Person
+    {
+        public string Name{get;set;}
+        public string Email{get;set;}
+        public int Age {get;set;}
+    }
+    public class PersonValidator:AbstractValidator<Person>{
+        public PersonValidator(){
+            RuleFor(n => n.Name).MinimumLength(1).MaximumLength(50).WithErrorCode("NameBetween1And50");
+            RuleFor(n => n.Email).EmailAddress().WithErrorCode("EmailMustContainAtChar");
+            RuleFor(n => n.Age).InclusiveBetween(0,120).WithErrorCode("AgeBetween0and120");
+        }
+    }
+}
