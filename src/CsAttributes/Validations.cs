f003f121fb46531642a3bd759fd55f6342d32cf4
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-01-28 20:48:17 +0100

    Using some copy pasted code

--- src/CsAttributes/Validations.cs
+++ src/CsAttributes/Validations.cs
@@ -1,32 +1,78 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 
 namespace CsAttributes
 {
+    /// see https://github.com/reustmd/DataAnnotationsValidatorRecursive
     class Validations
     {
         public static IEnumerable<ValidationResult> Validate(Person o)
         {
             var validationResults = new List<ValidationResult>();
-            var ivalidationResults = new List<ValidationResult>();
-            if (!Validator.TryValidateObject(o, new ValidationContext(o), ivalidationResults, true))
+            TryValidateObjectRecursive(o, validationResults);
+            return validationResults;
+        }
+        private static bool TryValidateObject(object obj, ICollection<ValidationResult> results)
+        {
+            return Validator.TryValidateObject(obj, new ValidationContext(obj), results, true);
+        }
+        private static bool TryValidateObjectRecursive<T>(T obj, List<ValidationResult> results)
+        {
+
+            bool result = TryValidateObject(obj, results);
+
+            var properties = obj.GetType().GetProperties().Where(prop => prop.CanRead
+                && prop.GetIndexParameters().Length == 0).ToList();
+
+            foreach (var property in properties)
             {
-                validationResults.AddRange(ivalidationResults);
-            }
-            for (int i = 0; i < o.Bookings.Count; i++)
-            {
-                var innerValidationResults = new List<ValidationResult>();
-                var innerV = o.Bookings[i];
-                if (!Validator.TryValidateObject(innerV, new ValidationContext(innerV), innerValidationResults, true))
+                if (property.PropertyType == typeof(string) || property.PropertyType.IsValueType) continue;
+
+                var value = property.GetValue(obj);
+
+                if (value == null) continue;
+
+                var asEnumerable = value as IEnumerable;
+                if (asEnumerable != null)
                 {
-                    validationResults.AddRange(innerValidationResults.Select(v=>new ValidationResult(v.ErrorMessage,
-                        v.MemberNames.Select(mn=>"Bookings["+i+"]."+mn).ToArray())).ToArray());
+                    var i = 0;
+                    foreach (var enumObj in asEnumerable)
+                    {
+                        var nestedResults = new List<ValidationResult>();
+                        if (!TryValidateObjectRecursive(enumObj, nestedResults))
+                        {
+                            result = false;
+                            foreach (var validationResult in nestedResults)
+                            {
+                                PropertyInfo property1 = property;
+                                results.Add(new ValidationResult(validationResult.ErrorMessage,
+                                    validationResult.MemberNames.Select(x => $"{property1.Name}[{i}].{x}").ToArray()));
+                            }
+                        };
+                        i++;
+                    }
+                }
+                else
+                {
+                    var nestedResults = new List<ValidationResult>();
+                    if (!TryValidateObjectRecursive(value, nestedResults))
+                    {
+                        result = false;
+                        foreach (var validationResult in nestedResults)
+                        {
+                            PropertyInfo property1 = property;
+                            results.Add(new ValidationResult(validationResult.ErrorMessage,
+                                validationResult.MemberNames.Select(x => property1.Name + '.' + x)));
+                        }
+                    }
                 }
             }
 
-            return validationResults;
+            return result;
         }
         public static string ToString(IEnumerable<ValidationResult> validations)
         {
