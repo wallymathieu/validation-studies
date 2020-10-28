c21a279f72cb142c53aa0da859b3780afab87638
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-10-28 20:34:25 +0100

    Split into files

--- src/CsGenericValidations/ValidationResult.cs
+++ src/CsGenericValidations/ValidationResult.cs
@@ -4,8 +4,8 @@ namespace CsGenericValidations
 {
     public abstract class ValidationResult<TSuccess,TFailure> : OneOfBase<ValidationResult<TSuccess,TFailure>.Success, ValidationResult<TSuccess,TFailure>.Failure>
     {
-        public class Success : ValidationResult<TSuccess,TFailure> { public TSuccess Value { get; protected internal set; } }  
-        public class Failure  : ValidationResult<TSuccess,TFailure> { public TFailure Value { get; protected internal set; } }
+        public class Success : ValidationResult<TSuccess,TFailure> { public new TSuccess Value { get; protected internal set; } }  
+        public class Failure  : ValidationResult<TSuccess,TFailure> { public new TFailure Value { get; protected internal set; } }
 
         public bool IsValid => this is Success;
 
