00cd072816a7ca1052a0dbf073c35680c3333b0b
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2020-10-29 07:19:30 +0100

    No need for separate Validations helper class

--- src/CsGenericValidations/ValidatePersonTests.cs
+++ src/CsGenericValidations/ValidatePersonTests.cs
@@ -1,4 +1,9 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+using System.Linq;
+using OneOf;
 using Xunit;
 
 namespace CsGenericValidations
@@ -13,32 +18,50 @@ namespace CsGenericValidations
         [Fact]
         public void Given_badBooking()
         {
-            Assert.Equal("DescriptionBetween1And50", Validations.ToString(
+            Assert.Equal("DescriptionBetween1And50", ToString(
                 Booking.Create(description:"" )));
         }
         [Fact]
         public void Given_badName()
         {
-            Assert.Equal("NameBetween1And50", Validations.ToString(Person.Create(name: "", age: 25, email: "bob@gmail.com",bookings:Enumerable.Empty<Booking>())));
+            Assert.Equal("NameBetween1And50", ToString(Person.Create(name: "", age: 25, email: "bob@gmail.com",bookings:Enumerable.Empty<Booking>())));
         }
         [Fact]
         public void Given_badEmail()
         {
             Assert.Equal("EmailMustContainAtChar",
-                Validations.ToString(Person.Create(name: "Bob", age: 25, email: "bademail",
+                ToString(Person.Create(name: "Bob", age: 25, email: "bademail",
                     bookings: Enumerable.Empty<Booking>())));
         }
         [Fact]
         public void Given_badAge()
         {
-            Assert.Equal("AgeBetween0and120", Validations.ToString(
+            Assert.Equal("AgeBetween0and120", ToString(
                 Person.Create( name : "Bob", age : 150, email : "bob@gmail.com", bookings: Enumerable.Empty<Booking>() )));
         }
         [Fact]
         public void Given_badEverything()
         {
-            Assert.Equal("NameBetween1And50;EmailMustContainAtChar;AgeBetween0and120", 
-                Validations.ToString(Person.Create( name : "", age : 150, email : "bademail", bookings: Enumerable.Empty<Booking>() )));
+            Assert.Equal(ToString(ValidationResult<Person,ReadOnlyCollection< DomainErrors>>
+                    .NewFailure(new []
+                        {
+                            DomainErrors.NameBetween1And50,DomainErrors.EmailMustContainAtChar,DomainErrors.AgeBetween0and120
+                        }.ToList().AsReadOnly())), 
+                ToString(Person.Create( name : "", age : 150, email : "bademail", bookings: Enumerable.Empty<Booking>() )));
+        }
+
+        private static string ToString<TSuccess,TFailure>(OneOfBase<ValidationResult<TSuccess, TFailure>.Success, ValidationResult<TSuccess, TFailure>.Failure> validations)
+        {
+            return validations.Match<string>(success => "Success", failure =>
+                failure.Value is IEnumerable
+                    ? string.Join(";", ValuesToString((IEnumerable)failure.Value))
+                    : failure.Value.ToString()
+            );
+        }
+
+        private static IEnumerable<string> ValuesToString(IEnumerable enumerable )
+        {
+            return from f in enumerable.Cast<object>() select f.ToString();
         }
     }
 }
