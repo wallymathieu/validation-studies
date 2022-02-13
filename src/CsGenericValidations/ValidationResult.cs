466c96d449c4d7d625a9fbb8f7d3625d3f1d0aed
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2022-02-13 17:13:28 +0100

    Update to .net6

--- src/CsGenericValidations/ValidationResult.cs
+++ src/CsGenericValidations/ValidationResult.cs
@@ -2,15 +2,16 @@ using OneOf;
 
 namespace CsGenericValidations
 {
-    public abstract class ValidationResult<TSuccess,TFailure> : OneOfBase<ValidationResult<TSuccess,TFailure>.Success, ValidationResult<TSuccess,TFailure>.Failure>
+    public class ValidationResult<TSuccess,TFailure> : OneOfBase<TSuccess,TFailure>
     {
-        public class Success : ValidationResult<TSuccess,TFailure> { public new TSuccess Value { get; protected internal set; } }  
-        public class Failure  : ValidationResult<TSuccess,TFailure> { public new TFailure Value { get; protected internal set; } }
+        public bool IsValid => this.IsT0;
 
-        public bool IsValid => this is Success;
+        public static ValidationResult<TSuccess, TFailure> NewSuccess(TSuccess success) =>  new( success);
 
-        public static ValidationResult<TSuccess, TFailure> NewSuccess(TSuccess success) => new Success { Value = success };
+        public static ValidationResult<TSuccess, TFailure> NewFailure(TFailure failure) => new (failure);
 
-        public static ValidationResult<TSuccess, TFailure> NewFailure(TFailure failure) => new Failure { Value = failure };
+        protected ValidationResult(OneOf<TSuccess,TFailure> input) : base(input)
+        {
+        }
     }
 }
