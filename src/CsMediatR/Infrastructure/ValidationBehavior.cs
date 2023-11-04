05f83cd374e60f8a418cce4e9a60bbc753ee2720
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2023-11-04 09:49:22 +0200

    net7

--- src/CsMediatR/Infrastructure/ValidationBehavior.cs
+++ src/CsMediatR/Infrastructure/ValidationBehavior.cs
@@ -10,8 +10,9 @@ public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<
     where TRequest : ICommand<TResponse>
 {
     private readonly IValidator<TRequest>[] _validators;
-    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators.ToArray(); 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators.ToArray();
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (!_validators.Any()) return await next();
         var context = new ValidationContext<TRequest>(request);
